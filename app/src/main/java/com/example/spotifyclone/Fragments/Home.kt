8482f08a3b72950ea5dd51937343b2daf7dc61d2
Josebias Neto <josebiasbsneto@gmail.com>
2021-07-02 15:35:12 -0300

    Create recycler view vertical

--- app/src/main/java/com/example/spotifyclone/Fragments/Home.kt
+++ app/src/main/java/com/example/spotifyclone/Fragments/Home.kt
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.SimpleAdapter
+import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.spotifyclone.R
+import model.Categoria
+import com.example.spotifyclone.R.layout.categoria_item
 
 
 class Home : Fragment() {
+
+    private lateinit var categoriaAdapter: CategoriaAdapter
 
     companion object {
         fun newInstance(): Home{
@@ -29,9 +39,38 @@ class Home : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        val categorias: MutableList<Categoria> = ArrayList()
+        for (c in 0..4){
+            val categoria = Categoria()
+            categoria.titulo = "Categoria$c"
+
+            categorias.add(categoria)
+        }
+
+
+        categoriaAdapter = CategoriaAdapter(categorias)
+        view.findViewById<RecyclerView>(R.id.recycler_view_categorias).adapter = categoriaAdapter
+        view.findViewById<RecyclerView>(R.id.recycler_view_categorias).layoutManager = LinearLayoutManager(context)
 
 
     }
 
-    private inner class CategoriaAdapter(private val categorias)
+    private inner class CategoriaAdapter(private val categorias: MutableList<Categoria>): RecyclerView.Adapter<CategoriaHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriaHolder {
+            return CategoriaHolder(layoutInflater.inflate(R.layout.categoria_item, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: CategoriaHolder, position: Int) {
+            val categoria = categorias[position]
+            holder.bind(categoria)
+        }
+
+        override fun getItemCount(): Int = categorias.size
+    }
+
+    private inner class CategoriaHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        fun bind(categoria: Categoria){
+            itemView.findViewById<TextView>(R.id.tv_titulo).text = categoria.titulo
+        }
+    }
 }
